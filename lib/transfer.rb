f7d4f63c972b8fdf9640dfdf82416b3976abd4b5
Kazumi Karbowski <kazkazinus@gmail.com>
2018-05-17 02:50:16 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,6 +20,7 @@ class Transfer
   def execute_transaction
     @sender.balance = @sender.balance - @amount
     @receiver.balance = @receiver.balance + @amount
+    @status == complete
     if @sender.valid? == false
       "Transaction rejected. Please check your account balance."
       @status = "rejected"
