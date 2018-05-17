f03742f9a74b564506c9820bfcfd932507796c2e
Kazumi Karbowski <kazkazinus@gmail.com>
2018-05-17 02:29:02 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,4 +1,11 @@
 class Transfer
 
+  attr_accessor :transfer_amount, :sender, :receiver, :status
+  def initialize(transfer_amount)
+    @transfer_amount = transfer_amount
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+  end
 
 end
