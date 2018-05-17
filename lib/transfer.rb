8700f72491fadb51418cbf31045f0b566c8a138a
Kazumi Karbowski <kazkazinus@gmail.com>
2018-05-17 02:55:09 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
   end
 
   def execute_transaction
-    
+
     @sender.balance = @sender.balance - @amount
     @receiver.balance = @receiver.balance + @amount
     @status = "complete"
